67d694b3aac449eba6f50a5fa615724819dd7907
danilodrobnjak <61749916+danilodrobnjak@users.noreply.github.com>
2021-06-10 14:07:19 +0200

    dodat parser

--- chat/Server/MainServer.cpp
+++ chat/Server/MainServer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Server.h"
+#include "Parser.h"
 
 #define DEFAULT_PORT "27015"
 #define SERVER_NAME "localhost"
@@ -11,16 +12,27 @@ public:
 	parser(int argv, char** argc);
 	bool is_input_valid()
 };
-
+*/
 void usage()
 {
-	const char* message = "Usage:\n - -p port"
+	const char* message = "Usage:\n  MainServer [--port -p] portValue\n []";
 }
-*/
+
 
 int main(int argv, char** argc) {
 
 	bool initWSA = false; 
+
+	
+	//Parser p(argv, argc);
+	try
+	{
+		Parser p(argv, argc);
+	}
+	catch (E_Parser& it)
+	{
+		std::cout << it<<std::endl;
+	}
 
 	//parser p(argv);
 	//if (!p.is_input_valid())
